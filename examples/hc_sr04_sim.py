216039de84a214d29917517fbdecb6f9d15b07d0
Saruccio <saruccio.culmone@yahoo.it>
2020-07-28 08:06:22 +0200

    Adapted to the new Sensor reading functions. All convertions to plot read points into the external coordinate system are now done outside the sensor.

--- examples/hc_sr04_sim.py
+++ examples/hc_sr04_sim.py
@@ -28,6 +28,7 @@ import shapes
 from flatland import FlatLand
 import geom_utils as geom
 from sensor import Sensor
+import numpy as np
 
 
 # Compose the simulated environment
@@ -73,7 +74,7 @@ logger.info("Bench points= {}".format(sim_env.size()))
 # S1 - HC-SR04
 S1 = Sensor(name="HC-SR04_01", beam=30, range=50)
 S1.load_env(sim_env.venv)
-S1.move((175, 300), 45)
+S1.place((175, 300), 45)
 
 # S2 - HC-SR04
 S2 = Sensor(name="HC-SR04_02", beam=45, range=90)
@@ -90,23 +91,43 @@ sim_env.add_sensors(S1, S2)
 S1.plot()
 S1.plot_surroundings()
 
-meas_data1, meas_point1, real_point1 = S1.read()
-logger.info("Dist1= {}".format(meas_data1[0]))
-logger.info("Point1= {}".format(meas_point1))
+meas_point1 = S1.read()
+logger.info("Dist1= {}".format(meas_point1[0]))
+
+# Transform reading in rectangular coordinates
+meas_rect_point1 = geom.pol2cart(meas_point1[0], np.deg2rad(meas_point1[1]))
+
+# Move the measured point into the external coordinate system
+S1x, S1y = S1.position
+S1alpha = S1.orientation # radiant since its the internal value
+S1_sys = (S1x, S1y, S1alpha, True) # Orientation in rad so flag is True
+ext_meas_point1 = geom.localpos_to_globalpos(meas_rect_point1, S1_sys)
 
 # Plot the measured point and the nearestpoint that is detected
-geom.plot_segment(S1.position, meas_point1)
-geom.plot_point(meas_point1)
-geom.plot_point(real_point1, pen_trait="+")
+geom.plot_segment(S1.position, ext_meas_point1)
+geom.plot_point(ext_meas_point1)
+
+
+# Plot the really detected point, taken from internal sensor structure
+real_rect_point1 = geom.pol2cart(S1.detected_point[0],
+                                 np.deg2rad(S1.detected_point[1]))
+ext_real_point1 = geom.localpos_to_globalpos(real_rect_point1, S1_sys)
+geom.plot_point(ext_real_point1, pen_trait="+")
 
 # S2 scan the space in front of it
-S2.move((220, 400), -90)
+# Position and orientation of the S2 sysref
+# Note that orientation is espressed in degree -> flag is False
+S2_sys = (220, 400, -90, False)
+S2.place((S2_sys[0], S2_sys[1]), S2_sys[2])
 S2.plot_surroundings()
 scan_points = S2.scan(-90, 90)
 
-# Get detected points ant plot them
-scan_meas = [t[1] for t in  scan_points]
-geom.plot(scan_meas)
+# Transform detected points in rectangular coordinates
+scan_meas = [geom.pol2cart(ppoint[0], np.deg2rad(ppoint[1])) for ppoint in scan_points]
+# Calculate points in the esternal sysref
+ext_scan_meas = [geom.localpos_to_globalpos(meas_point, S2_sys) for meas_point in scan_meas]
+
+geom.plot(ext_scan_meas)
 
 # Show overall picture
 sim_env.show()
